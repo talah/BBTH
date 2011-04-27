928ae55986a72f9ab52fae41678e622813d69047
Evan Wallace <evan_wallace@brown.edu>
2011-04-26 22:08:52 -0400

    added collision detection code

---
+++ src/bbth/collision/Point.java
@@ -0,0 +1,14 @@
+package bbth.collision;
+
+public class Point {
+	public float x;
+	public float y;
+
+	public Point() {
+	}
+
+	public Point(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+}
