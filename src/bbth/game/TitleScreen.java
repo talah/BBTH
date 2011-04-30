51b52829ae39240937b84be94cf8af5ef70ca1b0
thuhe <tala_huhe@brown.edu>
2011-04-30 18:30:58 -0400

    Let's pretend this works

--- src/bbth/game/TitleScreen.java
+++ src/bbth/game/TitleScreen.java
@@ -3,12 +3,13 @@ package bbth.game;
 import android.graphics.Color;
 import bbth.ui.Anchor;
 import bbth.ui.UILabel;
-import bbth.ui.UIScrollView;
+import bbth.ui.UIProgressBar;
 import bbth.ui.UIView;
 
-public class TitleScreen extends UIScrollView {
+public class TitleScreen extends UIView {
 	
 	private UILabel greeting;
+	private UIProgressBar progressBar;
 	
 	public TitleScreen(Object tag)
 	{
@@ -18,7 +19,13 @@ public class TitleScreen extends UIScrollView {
 		greeting.setTextColor(Color.WHITE);
 		greeting.setPosition(160.f, 90.f);
 		greeting.setTextSize(30.f);
-		
 		addSubview(greeting);
+		
+		//
+		progressBar = new UIProgressBar(greeting, greeting, tag);
+		progressBar.setAnchor(Anchor.CENTER_CENTER);
+		progressBar.setSize(30, 20);
+		progressBar.setPosition(160, 90);
+		addSubview(progressBar);
 	}
 }
