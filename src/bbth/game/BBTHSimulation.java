cc56393f262b772c3b01b149d891d98315a78e12
thuhe <tala_huhe@brown.edu>
2011-05-03 16:03:49 -0400

    Whoops, need accessors

--- src/bbth/game/BBTHSimulation.java
+++ src/bbth/game/BBTHSimulation.java
@@ -1,13 +1,17 @@
 package bbth.game;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import bbth.net.simulation.LockStepProtocol;
 import bbth.net.simulation.Simulation;
 
 public class BBTHSimulation extends Simulation {
-
+	private Unit test_unit;
+	
 	public BBTHSimulation(LockStepProtocol protocol) {
 		super(6, 0.1f, 2, protocol);
 		
+		test_unit = new Unit();
 	}
 
 	@Override
@@ -24,5 +28,10 @@ public class BBTHSimulation extends Simulation {
 
 	@Override
 	protected void update(float seconds) {
+		test_unit.update(seconds);
+	}
+	
+	public void draw(Canvas canvas) {
+		test_unit.draw(canvas);
 	}
 }
