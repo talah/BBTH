2e94a0ddb2bffa9c16f6f4ccc1810dbfb59e472d
greywhind <npartlan@techhouse.org>
2011-05-12 07:34:42 -0400

    Testing more pathfinding in AI.

--- gameSrc/bbth/game/ai/OffensiveAI.java
+++ gameSrc/bbth/game/ai/OffensiveAI.java
@@ -80,7 +80,7 @@ public class OffensiveAI extends UnitAI {
 		ycomp = m_flock_dir.y;
 
 		// Calculate somewhere to go if it's a leader.
-		if (!flock.hasLeader(entity)) {
+		//if (!flock.hasLeader(entity)) {
 			float goal_x = BBTHSimulation.GAME_WIDTH/2.0f;
 			float goal_y = 0;
 			if (entity.getTeam() == Team.SERVER) { 
@@ -136,7 +136,7 @@ public class OffensiveAI extends UnitAI {
 			float objectiveweighting = getObjectiveWeighting();
 			xcomp += objectiveweighting * FloatMath.cos(angle);
 			ycomp += objectiveweighting * FloatMath.sin(angle);
-		}
+		//}
 
 		float wanteddir = MathUtils.getAngle(0, 0, xcomp, ycomp);
 
