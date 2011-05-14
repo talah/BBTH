d22ffa8af2438d60bd50af97e3ee3a140666722f
Justin Ardini <jardini@cs.brown.edu>
2011-05-14 00:34:14 -0400

    pushed broken stuff, now fixed

--- gameSrc/bbth/game/Song.java
+++ gameSrc/bbth/game/Song.java
@@ -10,8 +10,7 @@ public enum Song {
 	RETRO(R.raw.retrobit, R.xml.track2),
 	GERUDO(R.raw.gerudovalley, R.xml.track2),
 	SONG_OF_STORMS_TECHNO(R.raw.stormoftechno, R.xml.track2),
-	MISTAKE_THE_GETAWAY(R.raw.mistakethegetaway, R.xml.track2),
-	HIFI(R.raw.hifi, R.xml.track2);
+	MISTAKE_THE_GETAWAY(R.raw.mistakethegetaway, R.xml.track2);
 	
 	private Song(int song, int track) {
 		songId = song;
