90f361696d066462312f238470815ed5e3042b54
greywhind <npartlan@techhouse.org>
2011-05-15 18:36:19 -0400

    Single player AI now spawns random walls!

--- gameSrc/bbth/game/ai/PlayerAI.java
+++ gameSrc/bbth/game/ai/PlayerAI.java
@@ -18,8 +18,11 @@ public class PlayerAI {
 	
 	private float m_difficulty = 1.0f;
 	private boolean m_spawned_this_beat = false;
+	private boolean m_walled_this_beat;
+	private BBTHSimulation m_simulation;
 	
-	public PlayerAI(Player player, Player enemy, BeatTrack beatTrack, float difficulty) {
+	public PlayerAI(BBTHSimulation simulation, Player player, Player enemy, BeatTrack beatTrack, float difficulty) {
+		m_simulation = simulation;
 		m_player = player;
 		m_enemy = enemy;
 		m_beats = beatTrack;
@@ -27,24 +30,26 @@ public class PlayerAI {
 	}
 	
 	public void update(float seconds) {
-		/*elapsedTime += seconds;
-		if (elapsedTime > DEBUG_SPAWN_TIMER) {
-			elapsedTime -= DEBUG_SPAWN_TIMER;
-			m_player.spawnUnit(BBTHSimulation.randInRange(0 + BeatTrack.BEAT_TRACK_WIDTH, BBTHSimulation.GAME_WIDTH),
-					BBTHSimulation.GAME_HEIGHT - 50);
-		}*/
-		
 		if (m_beats.getTouchZoneBeat() == BeatType.TAP && !m_spawned_this_beat) {
 			if (MathUtils.randInRange(0, 100) < m_difficulty * 100.0f) {
 				m_player.spawnUnit(BBTHSimulation.randInRange(0 + BeatTrack.BEAT_TRACK_WIDTH, BBTHSimulation.GAME_WIDTH),
 						BBTHSimulation.GAME_HEIGHT - 50);
 			}
 			m_spawned_this_beat  = true;
-		} else if (m_beats.getTouchZoneBeat() == BeatType.HOLD) {
+			m_walled_this_beat = false;
+		} else if (m_beats.getTouchZoneBeat() == BeatType.HOLD && !m_walled_this_beat) {
 			m_spawned_this_beat = false;
-			// TODO: spawn walls
+			m_walled_this_beat = true;
+			if (MathUtils.randInRange(0, 100) < m_difficulty * 100.0f) {
+				m_player.startWall(BBTHSimulation.randInRange(0 + BeatTrack.BEAT_TRACK_WIDTH, BBTHSimulation.GAME_WIDTH/3.0f),
+						BBTHSimulation.randInRange(BBTHSimulation.GAME_HEIGHT - 50, BBTHSimulation.GAME_HEIGHT - 250));
+				m_player.updateWall(BBTHSimulation.randInRange(0 + BeatTrack.BEAT_TRACK_WIDTH + BBTHSimulation.GAME_WIDTH * .66f, BBTHSimulation.GAME_WIDTH),
+						BBTHSimulation.randInRange(BBTHSimulation.GAME_HEIGHT - 50, BBTHSimulation.GAME_HEIGHT - 250));
+				m_simulation.generateWall(m_player);
+			}
 		} else if (m_beats.getTouchZoneBeat() == BeatType.REST) {
 			m_spawned_this_beat = false;
+			m_walled_this_beat = false;
 		}
 	}
 }
