20e1183805365de2b8eb519fd82172e2694a1c31
greywhind <npartlan@techhouse.org>
2011-05-15 18:35:11 -0400

    Simulation now kills units properly when they reach a base.

--- gameSrc/bbth/game/BBTHSimulation.java
+++ gameSrc/bbth/game/BBTHSimulation.java
@@ -309,7 +309,7 @@ public class BBTHSimulation extends Simulation implements UnitManager {
 					serverPlayer.adjustHealth(-10);
 				}
 
-				this.notifyUnitDead(u);
+				u.takeDamage(u.getHealth());
 			}
 		}
 		accel.getUnitsInAABB(cr.left, cr.top, cr.right, cr.bottom, cachedUnits);
@@ -319,7 +319,7 @@ public class BBTHSimulation extends Simulation implements UnitManager {
 					clientPlayer.adjustHealth(-10);
 				}
 
-				this.notifyUnitDead(u);
+				u.takeDamage(u.getHealth());
 			}
 		}
 
@@ -395,18 +395,18 @@ public class BBTHSimulation extends Simulation implements UnitManager {
 
 	@Override
 	public void notifyUnitDead(Unit unit) {
-		for (int i = 0; i < 10; i++) {
-			float angle = MathUtils.randInRange(0, 2 * MathUtils.PI);
-			float xVel = MathUtils.randInRange(25.f, 50.f)
-					* FloatMath.cos(angle);
-			float yVel = MathUtils.randInRange(25.f, 50.f)
-					* FloatMath.sin(angle);
-
-			BBTHSimulation.PARTICLES.createParticle().circle()
-					.velocity(xVel, yVel).shrink(0.1f, 0.15f).radius(3.0f)
-					.position(unit.getX(), unit.getY())
-					.color(unit.getTeam().getRandomShade());
-		}
+//		for (int i = 0; i < 10; i++) {
+//			float angle = MathUtils.randInRange(0, 2 * MathUtils.PI);
+//			float xVel = MathUtils.randInRange(25.f, 50.f)
+//					* FloatMath.cos(angle);
+//			float yVel = MathUtils.randInRange(25.f, 50.f)
+//					* FloatMath.sin(angle);
+//
+//			BBTHSimulation.PARTICLES.createParticle().circle()
+//					.velocity(xVel, yVel).shrink(0.1f, 0.15f).radius(3.0f)
+//					.position(unit.getX(), unit.getY())
+//					.color(unit.getTeam().getRandomShade());
+//		}
 
 		serverPlayer.units.remove(unit);
 		clientPlayer.units.remove(unit);
