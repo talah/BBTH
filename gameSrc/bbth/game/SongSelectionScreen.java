13224dc8d39211ad6df916f8687936180a9b3e8b
Evan Wallace <evan_wallace@brown.edu>
2011-05-15 20:00:33 -0400

    song selection screen touchups

--- gameSrc/bbth/game/SongSelectionScreen.java
+++ gameSrc/bbth/game/SongSelectionScreen.java
@@ -40,14 +40,14 @@ public class SongSelectionScreen extends UIScrollView implements
 		this.addSubview(makeButton(Song.JAVLA_SLADDER, "Javla Sladder", y++));
 		this.addSubview(makeButton(Song.ODINS_KRAFT, "Odin's Kraft", y++));
 		this.addSubview(makeButton(Song.MIGHT_AND_MAGIC, "Might and Magic", y++));
+		this.setContentRect(0, 0, BBTHGame.WIDTH, BBTHGame.HEIGHT / 2 + (y - 1) * 50);
 	}
 
 	private UIButton makeButton(Song song, String title, int idx) {
 		UIButton button = new UIButton(title, song);
 		button.setAnchor(Anchor.CENTER_CENTER);
-		button.setSize(BBTHGame.WIDTH * 0.75f, 45);
-		button.setPosition(BBTHGame.WIDTH / 2, BBTHGame.HEIGHT / 2 + (idx - 1)
-				* 65);
+		button.setSize(BBTHGame.WIDTH * 0.6f, 40);
+		button.setPosition(BBTHGame.WIDTH / 2, BBTHGame.HEIGHT / 2 + (idx - 1) * 50);
 		button.setButtonDelegate((BBTHGame.IS_SINGLE_PLAYER) ? this : delegate);
 		return button;
 	}
