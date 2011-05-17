3861af21382c05faf020b1ac05c54fea34332d5d
Evan Wallace <evan_wallace@brown.edu>
2011-05-17 02:05:33 -0400

    compressed song selection screen to avoid scrolling

--- gameSrc/bbth/game/SongSelectionScreen.java
+++ gameSrc/bbth/game/SongSelectionScreen.java
@@ -29,9 +29,10 @@ public class SongSelectionScreen extends UIScrollView implements
 		UILabel title = new UILabel("Song Selection", null);
 		title.setAnchor(Anchor.TOP_CENTER);
 		title.setTextSize(30.f);
-		title.setPosition(BBTHGame.WIDTH / 2.f, 80);
+		title.setPosition(BBTHGame.WIDTH / 2.f, 40);
 		this.addSubview(title);
 		this.setScrollsHorizontal(false);
+		this.setScrollsVertical(false);
 
 		int y = 0;
 		this.addSubview(makeButton(Song.DERP, "Derp", y++));
@@ -41,14 +42,14 @@ public class SongSelectionScreen extends UIScrollView implements
 		this.addSubview(makeButton(Song.JAVLA_SLADDAR, "Javla Sladdar", y++));
 		this.addSubview(makeButton(Song.ODINS_KRAFT, "Odin's Kraft", y++));
 		this.addSubview(makeButton(Song.MIGHT_AND_MAGIC, "Might and Magic", y++));
-		this.setContentRect(0, 0, BBTHGame.WIDTH, BBTHGame.HEIGHT / 2 + (y - 1) * 50);
+		this.setContentRect(0, 0, BBTHGame.WIDTH, 110 + y * 50);
 	}
 
 	private UIButton makeButton(Song song, String title, int idx) {
 		UIButton button = new UIButton(title, song);
-		button.setAnchor(Anchor.CENTER_CENTER);
+		button.setAnchor(Anchor.TOP_CENTER);
 		button.setSize(BBTHGame.WIDTH * 0.6f, 40);
-		button.setPosition(BBTHGame.WIDTH / 2, BBTHGame.HEIGHT / 2 + (idx - 1) * 50);
+		button.setPosition(BBTHGame.WIDTH / 2, 110 + idx * 50);
 		button.setButtonDelegate(this);
 		return button;
 	}
