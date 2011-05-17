cac909dff28839b05237be36d3c32080a50bdc62
Evan Wallace <evan_wallace@brown.edu>
2011-05-17 00:13:54 -0400

    moar tutorial

--- gameSrc/bbth/game/InteractiveTutorial.java
+++ gameSrc/bbth/game/InteractiveTutorial.java
@@ -63,6 +63,9 @@ public class InteractiveTutorial extends Tutorial implements UIButtonDelegate, U
 			addSubview(button);
 		}
 
+		public void onDrawSimulation(Canvas canvas) {
+		}
+
 		@Override
 		public void onClick(UIButton button) {
 		}
@@ -207,6 +210,7 @@ public class InteractiveTutorial extends Tutorial implements UIButtonDelegate, U
 	}
 
 	private class DrawWallStep extends Step {
+		private boolean isDragging;
 		private float startX;
 		private float startY;
 		private float endX;
@@ -226,6 +230,10 @@ public class InteractiveTutorial extends Tutorial implements UIButtonDelegate, U
 			canvas.drawText("When a beat has a tail,", x, y - 17, paint);
 			canvas.drawText("you can drag on the", x, y, paint);
 			canvas.drawText("grid to create a wall", x, y + 17, paint);
+		}
+
+		@Override
+		public void onDrawSimulation(Canvas canvas) {
 			paint.setColor(team.getWallColor());
 			canvas.drawLine(startX, startY, endX, endY, paint);
 		}
@@ -242,6 +250,7 @@ public class InteractiveTutorial extends Tutorial implements UIButtonDelegate, U
 			x -= GAME_X;
 			y -= GAME_Y;
 			if (x >= 0 && beat.onTouchDown((int) (songTime * 1000))) {
+				isDragging = true;
 				startX = endX = x;
 				startY = endY = y;
 			}
@@ -249,15 +258,20 @@ public class InteractiveTutorial extends Tutorial implements UIButtonDelegate, U
 
 		@Override
 		public void onTouchMove(float x, float y) {
-			endX = x;
-			endY = y;
+			if (isDragging) {
+				endX = x;
+				endY = y;
+			}
 		}
 
 		@Override
 		public void onTouchUp(float x, float y) {
-			endX = x;
-			endY = y;
-			BBTHSimulation.generateParticlesForWall(new Wall(startX, startY, endX, endY), team);
+			if (isDragging) {
+				endX = x;
+				endY = y;
+				BBTHSimulation.generateParticlesForWall(new Wall(startX, startY, endX, endY), team);
+				isDragging = false;
+			}
 		}
 	}
 
@@ -447,6 +461,7 @@ public class InteractiveTutorial extends Tutorial implements UIButtonDelegate, U
 		localPlayer.draw(canvas, team == Team.SERVER);
 		remotePlayer.draw(canvas, team == Team.SERVER);
 		BBTHSimulation.PARTICLES.draw(canvas, BBTHSimulation.PARTICLE_PAINT);
+		step.onDrawSimulation(canvas);
 		canvas.restore();
 		drawBeatTrack(canvas);
 		super.onDraw(canvas);
