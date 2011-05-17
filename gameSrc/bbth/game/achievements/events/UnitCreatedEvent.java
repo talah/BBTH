5d4f80d2ff2cdae32efd55550de34ab05c0724c9
greywhind <npartlan@techhouse.org>
2011-05-17 01:21:19 -0400

    Added UnitCreatedEvent field for unit and a getUnitType method.

--- gameSrc/bbth/game/achievements/events/UnitCreatedEvent.java
+++ gameSrc/bbth/game/achievements/events/UnitCreatedEvent.java
@@ -1,11 +1,23 @@
 package bbth.game.achievements.events;
 
 import bbth.game.*;
+import bbth.game.units.Unit;
+import bbth.game.units.UnitType;
 
 public class UnitCreatedEvent extends BBTHAchievementEvent {
 
+	private Unit m_unit;
+
 	public UnitCreatedEvent(Song song, Player localPlayer) {
 		super(song, localPlayer);
+	}
+	
+	public void set(Unit u) {
+		m_unit = u;
+	}
+
+	public UnitType getUnitType() {
+		return m_unit.getType();
 	}
 
 }
