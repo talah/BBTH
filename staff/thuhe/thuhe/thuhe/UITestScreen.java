d58785d7feefbcde84f514887aead2d8e20c1878
thuhe <tala_huhe@brown.edu>
2011-05-17 02:33:16 -0400

    Blue progress bars

--- staff/thuhe/thuhe/thuhe/UITestScreen.java
+++ staff/thuhe/thuhe/thuhe/UITestScreen.java
@@ -2,6 +2,7 @@ package thuhe.thuhe.thuhe;
 
 import bbth.engine.ui.Anchor;
 import bbth.engine.ui.UILabel;
+import bbth.engine.ui.UIProgressBar;
 import bbth.engine.ui.UISlider;
 import bbth.engine.ui.UISwitch;
 import bbth.engine.ui.UIView;
@@ -11,11 +12,18 @@ public class UITestScreen extends UIView {
 	private UISlider slider;
 	private UILabel label;
 	private UISwitch uiSwitch;
+	private UIProgressBar progressBar;
 	
 	public UITestScreen() {
 		this.setAnchor(Anchor.TOP_LEFT);
 		this.setSize(BBTHGame.WIDTH, BBTHGame.HEIGHT);
 		this.setPosition(0, 0);
+		
+		progressBar = new UIProgressBar();
+		progressBar.setAnchor(Anchor.CENTER_CENTER);
+		progressBar.setPosition(BBTHGame.WIDTH / 2, BBTHGame.HEIGHT / 2 - 160);
+		progressBar.setSize(150, 20);
+		this.addSubview(progressBar);
 		
 		slider = new UISlider(1.f, 5.f, 2.f);
 		slider.setAnchor(Anchor.CENTER_CENTER);
@@ -42,6 +50,9 @@ public class UITestScreen extends UIView {
 		super.onUpdate(seconds);
 		
 //		label.setText("Value: " + slider.getValue());
-		label.setText("Value: " + (uiSwitch.isOn() ? "ON" : "OFF"));
+//		label.setText("Value: " + (uiSwitch.isOn() ? "ON" : "OFF"));
+		label.setText("Value: " + progressBar.getProgress());
+		
+		progressBar.setProgress(progressBar.getProgress() + 0.01f);
 	}
 }
